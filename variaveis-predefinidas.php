a8984ad2f5952589ff8a5d297641f13ef7aa9c0e
Tiago Neves <rykarumstuler@gmail.com>
2021-03-29 17:48:52 -0300

    page ends

--- variaveis-predefinidas.php
+++ variaveis-predefinidas.php
@@ -20,6 +20,23 @@ function br()
  * 
  *  $_SERVER['SERVER_NAME'] -> Retorna o nome do HOST servidor.
  * 
+ *  $_SERVER['HTTP_HOST'] -> Retorna o HEAD do servidor;
+ * 
+ *  $_SERVER['REMOTE_ADDR'] -> Retorna o endereço IP que o usuário está vendo
+ *  
+ * $_SERVER['REMOTE_HOST'] -> Retorna o HOST que o usuário está vendo.
+ * 
+ * $_SERVER['REMOTE_PORT'] -> Retorna a PORT que o usuário está utilizando
+ * para se comunicar com o servirdor.
+ * 
+ * $_SERVER['SCRIPT_FILENAME'] -> Retorna o caminho absoluto desse script.
+ * 
+ * $_SERVER['SERVER_PORT'] -> Retorna a PORT do servidor.
+ * 
+ * $_SERVER['SCRIPT_NAME'] -> Retorna o nome do script
+ * 
+ * $_SERVER['SCRIPT_URI'] -> Retorna a URI da pagina atual. 
+ * 
  *  ==================================================
  *  $GLOBALS -> 
  *  
@@ -56,3 +73,19 @@ echo $_SERVER['SERVER_ADDR'];
 br();
 echo $_SERVER['SERVER_NAME'];
 br();
+echo $_SERVER['REMOTE_ADDR'];
+br();
+echo $_SERVER['REMOTE_HOST'];
+br();
+echo $_SERVER['REMOTE_PORT'];
+br();
+echo $_SERVER['SCRIPT_FILENAME'];
+br();
+echo $_SERVER['SCRIPT_URI'];
+br();
+echo $_SERVER; // É um array
+br();
+
+// =========================================
+//           PHP FORMS
+// =========================================
