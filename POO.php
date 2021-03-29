c014badaacb8a8201088e9e7606e62f2341de5cf
Tiago Neves <rykarumstuler@gmail.com>
2021-03-29 20:24:00 -0300

    POO page ends

--- POO.php
+++ POO.php
@@ -21,3 +21,50 @@ $tiago->age = 21;
 
 echo $tiago->age;
 $tiago->speak();
+
+
+class Dog
+{
+    public $legs = 4;
+    public function display()
+    {
+        echo $this->legs;
+    }
+}
+$d1 = new Dog();
+$d1->display();
+
+$d2 = new Dog();
+$d2->legs = 2;
+$d2->display();
+
+
+// PHP construtor de classes
+class Person2
+{
+    public function __construct() // Essa função é chamada quando o objecto
+    // é criado.
+    {
+        echo "Object created";
+    }
+}
+
+$p = new Person2(); // Vai informa que o objeto está criado.
+
+class Person3
+{
+    public $name;
+    public $age;
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function __destruct()
+    {
+        echo "Object created with sucessful.";
+    }
+}
+
+$p1 = new Person3('Tiago', 21);
