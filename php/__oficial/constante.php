850516c445a877e9f679c4e1f6f46bf54932b96d
Tiago Neves <rykarumstuler@gmail.com>
2021-04-04 23:39:51 -0300

    Constantes da documentaçao

---
+++ php/__oficial/constante.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Constantes: Para definir uma constante basta utilizar a função: 
+ * define() -> Dessa forma define-se uma constante. Por conversão as 
+ * constantes sempre recebem o seu nome em maiusculas.
+ * 
+ * No PHP existe as constantes mágicas, geradas a partir de extensões que 
+ * estão habilitadas mas podem ficar desabilitadas, recebe a notaçõa :
+ * __MAGICK__ . 
+ */
