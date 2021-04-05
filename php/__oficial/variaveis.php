4a2a384b35d310e74b98871a8a817270688db996
Tiago Neves <rykarumstuler@gmail.com>
2021-04-04 23:16:39 -0300

    Variaveis da documentaçao

---
+++ php/__oficial/variaveis.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Variáveis: As variáveis no php utiliza o $ para declarar.
+ * É possível referenciar uma variável colocando um e-comercial, dessa forma
+ * se a referência for modificada a variável também será. 
+ * 
+ * O escopo das variáveis em PHP é definido pelo escopo local, em que 
+ * se uma variápel é declarada em determinado local, então a mesma deve ser
+ * usada somente nesse local.
+ * Ah não ser utilizanod a palavra-chave global. Ou o array especial $GLOBALS
+ * 
+ * Utilizar a palavra-chave static antes da declaração da variável faz o valor
+ * da variável ser declarada  uma única vez, desta forma se a mesma sofrer uma
+ * modificação, ela permanecerá essa modificação.
+ * 
+ * Variáveis de font externas. Utilizando superglobais como $_POST e $_GET, 
+ * atribuindo o atributo name como a chave do array.
+ * 
+ */
